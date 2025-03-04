afc06a71cb7d92905bc7c2ed932f8551f1405758
Iliyas-shah <shahiliyas289@gmail.com>
2025-03-05 00:03:19 +0530

    created userSchema, model, and connected mongodb

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import mongoose from 'mongoose'
+import { connectDB } from './connections/user.js'
 
 dotenv.config()
 const app = express()
 const port = process.env.PORT
+const urlDB = process.env.urlDB
+
+connectDB(urlDB)
+.then(()=>console.log(`Database Connected`))
+.catch((err)=>console.log(err))
+
 
 app.listen(port,()=>console.log(`Server Started On Port ${port}`))
