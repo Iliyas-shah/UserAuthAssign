702f604892a039f7a64f10d7d40a72e327862d99
Iliyas-shah <shahiliyas289@gmail.com>
2025-03-06 20:46:16 +0530

    reset password code change

--- middlewares/user.js
+++ middlewares/user.js
@@ -4,13 +4,13 @@ export function checkJwtToken(req,res,next){
     try{
         const userToken = req.cookies
         if(Object.keys(userToken).length === 0) //check if there is no cookies
-            return res.status(404).json({message:"Jwt Token not available, Please Login"})
+            return res.status(401).json({message:"Jwt Token not available, Please Login"})
         if(!verifyUserToken(userToken.userAuthToken)) //check if there is an invalid jwt token
-            return res.status(404).json({message:"Invalid Jwt Token,Please Login"})
+            return res.status(401).json({message:"Invalid Jwt Token,Please Login"})
 
         const userDetails = verifyUserToken(userToken.userAuthToken)
         if(userDetails.ipAddress!=req.ip) //check if only authenticated ip address is sending a request
-            return res.status(404).json({messasge:"Authentication Failed, Please Login"})
+            return res.status(401).json({messasge:"Authentication Failed, Please Login"})
 
         req.userDetails = userDetails
     }catch(error){
